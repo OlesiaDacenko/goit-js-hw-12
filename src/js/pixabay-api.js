26c1f70865827484dca45bb354f7f4011439d261
Olesia Dacenko <o.khlivnuk@gmail.com>
2025-04-13 14:26:38 +0300

    my first commit for deploy

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,8 @@
+// У файлі pixabay-api.js зберігай функції для виконання HTTP-запитів:
+
+// getImagesByQuery(query, page). Ця функція повинна приймати два параметри
+//  query (пошукове слово, яке є рядком) та page (номер сторінки, яка є числом),
+//   здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
+export function getImagesByQuery(query, page) {
+  return (respons = axios());
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+// не памятаю чи треба так підключати
+
+/* <script type="module" src="./js/pixabay-api.js"></script>;
+<script type="module" src="./js/render-functions.js"></script>; */
